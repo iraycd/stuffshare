52f348b83f68541e781b887f44a757a31e29cc57
mmach900 <m.mach.90@gmail.com>
2019-01-25 15:13:39 +0100

    no message

--- src/Front-end/Web/Components/index.js
+++ src/Front-end/Web/Components/index.js
@@ -1,6 +1,6 @@
-import DropDownList from './FormComponent/components/DropDownList/index.jsx';
-import TextBox from './FormComponent/components/TextBox/index.jsx';
-import ButtonLoader from './FormComponent/components/ButtonLoader/index.jsx';
+import DropDownList from './FormComponent/Components/DropDownList/index.jsx';
+import TextBox from './FormComponent/Components/TextBox/index.jsx';
+import ButtonLoader from './FormComponent/Components/ButtonLoader/index.jsx';
 
 
 module.exports = { TextBox, DropDownList,ButtonLoader };
