401d3bc94fc7e6fd6f09c39f62c21edfcbd1ef94
mmach900 <m.mach.90@gmail.com>
2019-04-29 10:03:17 +0200

    no message

--- webpack_web_prod.config.js
+++ webpack_web_prod.config.js
@@ -1,28 +1,44 @@
 'use strict';
 
 const webpack = require('webpack');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+let find = require('find');
+let fs = require('fs');
 //         path = require('path');//
 //const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 //const extractLESS = new ExtractTextPlugin('./public/two.css');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './index.html',
+    template: './src/Front-end/Web/index.html',
     filename: 'index.html',
     inject: 'body'
 });
+
+
 const config = [{
-    performance: { hints: false },
+
     entry: {
-        "app": ['./dist/web/app.bundle.js', './dist/web/bundle.css',
+        "app": ['./src/Front-end/Web/app.jsx', './src/Front-end/Web/assets/vendor/bootstrap/bootstrap.min.css', './src/Front-end/Web/assets/vendor/icon-awesome/css/font-awesome.min.css', './src/Front-end/Web/assets//vendor/icon-hs/style.css', './src/Front-end/Web/assets/vendor/dzsparallaxer/dzsparallaxer.css', './src/Front-end/Web/assets/vendor/dzsparallaxer/dzsscroller/scroller.css', './src/Front-end/Web/assets/vendor/dzsparallaxer/advancedscroller/plugin.css', './src/Front-end/Web/assets/vendor/animate.css', './src/Front-end/Web/assets/vendor/hs-megamenu/src/hs.megamenu.css', './src/Front-end/Web/assets/vendor/hamburgers/hamburgers.min.css', './src/Front-end/Web/assets/css/unify.css', './src/Front-end/Web/assets/css/custom.css'
+            /*
+                ,'./src/Front-end/Web/assets/vendor/tether.min.js'
+                ,'./src/Front-end/Web/assets/vendor/bootstrap/bootstrap.min.js'
+                ,'./src/Front-end/Web/assets/vendor/hs-megamenu/src/hs.megamenu.js'
+                ,'./src/Front-end/Web/assets/vendor/dzsparallaxer/dzsparallaxer.js'
+                ,'./src/Front-end/Web/assets/vendor/dzsparallaxer/dzsscroller/scroller.js'
+                ,'./src/Front-end/Web/assets/vendor/dzsparallaxer/advancedscroller/plugin.js'
+                ,'./src/Front-end/Web/assets/vendor/typedjs/typed.min.js'
+              
+                ,'./src/Front-end/Web/assets/js/hs.core.js'
+                ,'./src/Front-end/Web/assets/js/components/hs.header.js'
+                ,'./src/Front-end/Web/assets/js/helpers/hs.hamburgers.js'
+                ,'./src/Front-end/Web/assets/js/components/hs.go-to.js'*/
         ]
         //,'./public/city_guide.scss','./public/awesomplete.css'
     },
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/src/Back-end/web',
         filename: '[name].bundle.js',
         publicPath: '/'
     },
@@ -32,7 +48,8 @@ const config = [{
     },
     module: {
         loaders: [
-           
+            { test: /\.js$/, loader: ['babel-loader'], exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: ['babel-loader' /*, "eslint-loader"*/], exclude: /node_modules/ },
             { // sass / scss loader for webpack
                 test: /\.(css|sass|scss)$/,
                 loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
@@ -48,11 +65,22 @@ const config = [{
 
 
     },
+    node: { fs: 'empty' },
+    resolve: {
+    
+        extensions: ['.js', '.jsx', '.css', '.sass', '.scss', '.png', '.jpg', '.gif', '.cur', '.woff', '.eot', '.ttf', '.svg', '.dtd', '.woff2'],
+    },
     plugins: [HtmlWebpackPluginConfig,
         new ExtractTextPlugin({ // define where to save the file
             filename: 'bundle.css',
             allChunks: true,
-        })
+        }), new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        
+
     ]
 
 }];
